980036d61b58174b0a3f0db7f7c0f86c6ddb9538
denise022sanchez-dot <denise022sanchez@gmail.com>
2025-11-01 18:28:22 -0500

    commit de prueba

--- conecta4/conecta4/conecta4.cpp
+++ conecta4/conecta4/conecta4.cpp
@@ -1,19 +1,31 @@
 // conecta4.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
+#pragma execution_charecter_set("utf-8")
 #include <iostream>
+#include <vector>
+using namespace std;
+
+
+const int FILAS = 6;
+const int COLUMNAS = 7;
+
+//Funcion para imprimir tablero
+
+void imprimirTablero(const vector<vector<char>>& tablero) {
+
+
+
+}
+
+
+
+
+
+
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
