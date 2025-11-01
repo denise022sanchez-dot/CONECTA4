090c02e2f49ab42990eee40369f16d1631c94dc5
denise022sanchez-dot <denise022sanchez@gmail.com>
2025-11-01 18:49:37 -0500

    Creacion de la funcion para Imprimir el tablero

--- conecta4/conecta4/conecta4.cpp
+++ conecta4/conecta4/conecta4.cpp
@@ -13,19 +13,29 @@ const int COLUMNAS = 7;
 //Funcion para imprimir tablero
 
 void imprimirTablero(const vector<vector<char>>& tablero) {
+    cout << "\n  ";
+    // Para recorrer las columnas (1 al 7)
+    for (int c = 0; c < COLUMNAS; c++) {
+        cout << c + 1 << " ";  //Para mostrar los numeros de la columna
+    }
+    cout << "\n";
 
 
+    //Para recorrer todas las filas
+    for (int f = 0; f < FILAS; f++) {
+        cout << "| ";
+        for (int c = 0; c < COLUMNAS; c++) {
+            cout << tablero[f][c] << " ";  /*Ciclo for anidado para mostrar en pantalla
+            el contenido de cada celda ".", "X", "O"*/
 
+        }
+        cout << "|\n";
+    }
+    cout << "-----------------\n";
 }
 
+int main() {
 
 
 
-
-
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
+        }
